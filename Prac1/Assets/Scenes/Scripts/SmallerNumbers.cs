dba92782d0911b658c11c0508d197a1b9ca0838e
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2020-10-02 08:15:21 -0700

    Parte final
    Por que te caise ayer Github? :(

---
+++ Prac1/Assets/Scenes/Scripts/SmallerNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallerNumbers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = { 8, 1, 2, 2, 3 };  /* {5, 8, 245, 3, 2, 45 }; Prueba*/
+
+        Debug.Log("Input: " + string.Join(", ",array));
+        
+        array = smallerNumbers(array);
+
+        Debug.Log("Output: " + string.Join(", ", array));
+    }
+
+    // Update is called once per frame
+    int[] smallerNumbers(int[] array)
+    {
+        int[] res = new int[array.Length];
+
+
+        for (int i = 0; i < array.Length; i++)
+            for (int j = 0; j < array.Length; j++)
+                if (array[i] > array[j])
+                    res[i]++;
+
+        return res;
+    }
+}
