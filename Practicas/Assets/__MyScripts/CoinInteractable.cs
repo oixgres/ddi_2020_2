69891c5a35daa89578c8e4a2e34c5780d56972f3
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2020-10-12 23:20:48 -0700

    Previo-Prac3
    Se cambio el nombre de Pract2 a Practicas, dado que todas las practicas estaran en distintas escenas dentro de un mismo proyecto

---
+++ Practicas/Assets/__MyScripts/CoinInteractable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinInteractable : Interactable
+{
+    Rigidbody rb;
+    public float torque;
+    bool interactionSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.interact();
+    }
+}
---
+++ Practicas/Assets/__MyScripts/Interactable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    bool inArea = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void interact()
+    { 
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inArea && Input.GetKeyDown(KeyCode.G))
+            interact();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        inArea = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+        inArea = false;
+    }
+}
