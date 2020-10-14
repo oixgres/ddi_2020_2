3f7227d17da70c5040d79816d632103163aefc52
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2020-10-14 14:57:42 -0700

    Avance2 - Practica3
    Se implemento la interaccion para cargar y soltar objetos, junto con la interaccion de recolectar objetos para aumentar puntuacion

---
+++ Practicas/Assets/__MyScripts/CarryObjects.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class CarryObjects :MonoBehaviour
+{
+    bool inArea = false, have = false;
+    Rigidbody rb;
+    MeshCollider md;
+
+    public Rigidbody character;
+    public Vector3 distance;
+    public Transform interactionZone;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        md = GetComponent<MeshCollider>();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L) && inArea)
+        {
+            Debug.Log("Levantando");
+            
+            have = !have;
+
+            if (have)
+            {
+                rb.transform.SetParent(interactionZone);
+                rb.transform.position += distance;
+                md.isTrigger = true;
+                rb.useGravity = false;
+                rb.isKinematic = true;
+            }
+            else
+            {
+                rb.transform.SetParent(null);
+                rb.transform.position -= distance;
+                md.isTrigger = false;
+                rb.useGravity = true;
+                rb.isKinematic = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        inArea = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+        inArea = false;
+    }
+}
---
+++ Practicas/Assets/__MyScripts/ScoreText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class ScoreText : MonoBehaviour
+{
+    public Text scoreText;
+    public double score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            score = Convert.ToDouble(scoreText.text);
+            score++;
+            scoreText.text = score.ToString("0");
+            Destroy(gameObject);
+        }
+    }
+}
