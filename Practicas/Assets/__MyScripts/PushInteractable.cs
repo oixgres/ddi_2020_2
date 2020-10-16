21ad5987d77f969f54259df278a2f4f9980e5161
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2020-10-15 21:19:50 -0700

    Avance4 - Practica3 - Extra
    Añadi una interaccion de empujar

---
+++ Practicas/Assets/__MyScripts/PushInteractable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushInteractable : Interactable
+{
+    public float pushspeed;
+    public Vector3 kickDirection;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void interact()
+    {
+        base.interact();
+        Debug.Log("Pushing...");
+
+        if (rb != null)
+            rb.AddForce(kickDirection * 100, ForceMode.Acceleration);
+    }
+}
