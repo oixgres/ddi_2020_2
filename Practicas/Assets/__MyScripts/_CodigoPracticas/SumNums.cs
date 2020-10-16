5180e0b3d94e18c917feeee8b131ba162d402d28
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2020-10-15 20:46:41 -0700

    Avance3 - Practica3
    Se implemento el ejercio de la practica

---
+++ Practicas/Assets/__MyScripts/_CodigoPracticas/SumNums.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SumNums : MonoBehaviour
+{
+    public int search = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] input = { 2, 7, 11, 15 };
+        int[] output = sumaDos(input, search);
+
+
+        Debug.Log(string.Join(",", input));
+        Debug.Log(string.Join(",", output));
+    }
+
+    public int[] sumaDos(int[] nums, int target)
+    {
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+        int key;
+
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            key = target - nums[i];
+
+            if (dictionary.ContainsKey(key))
+                return new int[] { dictionary[key], i };
+            
+            dictionary.Add(nums[i], i);
+        }
+        return null;
+    }
+}
