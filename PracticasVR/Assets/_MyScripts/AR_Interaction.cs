9c583ffc4f3e45360b0275e232619748a52dea50
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2020-12-01 21:04:11 -0800

    Practica7
    Practica7, se agregaron interacciones AR

---
+++ PracticasVR/Assets/_MyScripts/AR_Interaction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AR_Interaction : MonoBehaviour
+{
+    public string action;
+    public Animator animator;
+
+    private bool isActing = false;
+
+    void OnMouseDown()
+    {
+        isActing = !isActing;
+        Debug.LogWarning("CHANGE");
+    }
+
+    private void Update()
+    {
+        if (isActing)
+        {
+            animator.Play(action);
+            Debug.Log(action);
+        }
+        else
+            animator.Play("None");
+    }
+}
